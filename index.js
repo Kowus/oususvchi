7f56ca1225747d8cc79f47480c5ef60a49f19178
Kowus <barnabasnomo@gmail.com>
2016-10-07 19:48:53 +0000

    message validation

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var Mailgun = require('mailgun').Mailgun;
+var nodemailer = require('nodemailer');
+
+
 
 var app = express();
-// Setting up mailgun
-var api_key = '4876fd60bea6da809a90de5b4b70d0f7';
-var domain = 'https://api.mailgun.net/v3/sandbox1218837047e94fc4859105c50046700e.mailgun.org/messages';
-var sender = 'Mailgun Sandbox <postmaster@sandbox1218837047e94fc4859105c50046700e.mailgun.org>';
 
-
-var mg = new Mailgun({apiKey: api_key, domain: domain});
 
 app.use(bodyParser.json());         // To support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // To support URL-encoded bodies
@@ -20,30 +16,65 @@ app.use(bodyParser.urlencoded({     // To support URL-encoded bodies
 
 app.use('/', express.static(__dirname + '/'));
 
-app.post('/myapi', function(req, res){
+
+
+
+
+function handleRender(req, res) {
     
-    mg.sendText(sender, ['Barnabas Nomo <barnabasnomo@gmail.com>'], 
-        'New Enquiry From ' + req.body.name,
-        '\n\n' + req.body.message,
-        'Reply-to ' + req.body.my_email, {},
-        function(err) {
-            if(err) console.log('Oh blimey!: ' + err);
-            else console.log('Success');
+}
+
+
+
+app.post('/myapi', function(req, res){
+
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'ousumaily@gmail.com',
+            pass: 'JoshAkyi16'
         }
-    );
+    });
+    
+    var mailOptions = {
+        from: req.body.name + "" + "<"+req.body.my_email+">", // sender address
+        to: 'barnabas@elite-education.org, sodramatic@live.com, oususvchi@gmail.com', // list of receivers
+        subject: 'New email from ' + req.body.name + "" + "<"+req.body.my_email+">", // Subject line
+        text: '\n' + req.body.message, // plaintext body
+        //html: '<b>Hello world ?</b>' // html body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.json({yo: 'error'});
+        }else{
+            console.log('Message sent: ' + info.response);
+            console.log("\nName is " +req.body.name);
+            console.log("Email is " + req.body.my_email);
+            console.log("Message is " + req.body.message);
+            res.json({yo: info.response});
+
+        };
+});
 
 
-    /*
-    console.log("\nName is " +req.body.name);
-    console.log("Email is " + req.body.my_email);
-    console.log("Message is " + req.body.message);
-    */
+    
 });
 
 
 app.listen(3423, function(){
     console.log("my Api being called\n");
 });
+
+
+
+
+
+
+
+
+
 
 
 /*
