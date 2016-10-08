e72dc99a028d1d1b14822d421e9b2f61c1103333
Kowus <barnabasnomo@gmail.com>
2016-10-08 09:17:53 +0000

    added XMLhttprequest

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var nodemailer = require('nodemailer');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var API_Key = '1bf1a4c3a29abbb2f3d9c8c0ce6b872f5e65dabd1f2541c6ab9c8f995a1d';
 
@@ -9,13 +10,15 @@ var quickemailverification = require('quickemailverification').client(API_Key).q
 
 
 var app = express();
-
+var app2 = express();
 
 app.use(bodyParser.json());         // To support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // To support URL-encoded bodies
     extended: true
 }));
 
+var resultHolder = "";
+var reasonHolder = "";
 
 app.use('/', express.static(__dirname + '/'));
 
@@ -46,30 +49,40 @@ app.post('/myapi', function(req, res){
 
     quickemailverification.verify(req.body.my_email, function (err, response) {
         // Print response object
-        console.log(response.body);
+        console.log(response.body.result);
+        console.log(response.body.reason);
+        resultHolder = response.body.result;
+        reasonHolder = response.body.reason;
     });
+    if(resultHolder == 'valid'){
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log(error);
+                res.json({yo: 'error'});
+            }else{
+                console.log('Message sent: ' + info.response);
+                console.log("\nName is " +req.body.name);
+                console.log("Email is " + req.body.my_email);
+                console.log("Message is " + req.body.message);
+                res.json({yo: info.response});
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            console.log(error);
-            res.json({yo: 'error'});
-        }else{
-            console.log('Message sent: ' + info.response);
-            console.log("\nName is " +req.body.name);
-            console.log("Email is " + req.body.my_email);
-            console.log("Message is " + req.body.message);
-            res.json({yo: info.response});
-
-        };
-});
-
+            };
+        });
+    }else{
+        console.log('invalid email address' + reasonHolder);
+        document.getElementById("kowus").innerHTML = "New text!";
+        
+    }
 
     
 });
 
 
-app.listen(3423, function(){
-    console.log("my Api being called\n");
+
+
+var port = 3423;
+app.listen(port, function(){
+    console.log("listening on localhost:"+port+'\n');
 });
 
 
